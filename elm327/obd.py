46ec831ddd0803d6f8123a5bed10b85602f5333e
Francisco Ruiz <quiesagua@gmail.com>
2014-04-09 21:50:58 +0100

    Implemented wrappers for OBD commands and responses

---
+++ elm327/obd.py
@@ -0,0 +1,121 @@
+################################################################################
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Francisco Ruiz
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+################################################################################
+
+_OBD_RESPONSE_NO_DATA = "NO DATA"
+
+_OBD_RESPONSE_UNSUPPORTED_COMMAND = "?"
+
+_OBD_RESPONSE_ID = int('40', 16)
+
+
+_INT_TO_HEX_WORD_FORMATTER = "{:0=2X}"
+
+_INT_TO_HEX_WORD_FORMATTER_PRETTY = "{:0=#4x}"
+
+
+class ELMError(Exception):
+
+    pass
+
+
+class NoDataReceivedError(ELMError):
+
+    pass
+
+
+class UnsupportedCommandError(ELMError):
+
+    pass
+
+
+class OBDCommand(object):
+
+    def __init__(self, mode, pid):
+        self.mode = mode
+        self.pid = pid
+
+    def to_hex_words(self, pretty=False):
+        hex_words = (
+            _convert_int_to_hex_word(self.mode, pretty=pretty),
+            _convert_int_to_hex_word(self.pid, pretty=pretty),
+            )
+        return hex_words
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            raise NotImplementedError
+        return self.__dict__ == other.__dict__
+
+    def __repr__(self):
+        return "{}(mode={}, pid={})".format(
+            self.__class__.__name__,
+            *self.to_hex_words(True)
+            )
+
+
+def _convert_int_to_hex_word(i, pretty=False):
+    if pretty:
+        formatter = _INT_TO_HEX_WORD_FORMATTER_PRETTY
+    else:
+        formatter = _INT_TO_HEX_WORD_FORMATTER
+
+    return formatter.format(i)
+
+
+class OBDResponse(object):
+
+    def __init__(self, command, raw_data):
+        self.command = command
+        self.raw_data = tuple(raw_data)
+
+    @classmethod
+    def make(cls, response_raw):
+        if response_raw == _OBD_RESPONSE_NO_DATA:
+            raise NoDataReceivedError()
+
+        if response_raw == _OBD_RESPONSE_UNSUPPORTED_COMMAND:
+            raise UnsupportedCommandError()
+
+        response_words = _convert_raw_response_to_words(response_raw)
+
+        response_mode = response_words[0]
+        response_pid = response_words[1]
+        command = _parse_response_command(response_mode, response_pid)
+
+        raw_data = response_words[2:]
+
+        return cls(command, raw_data)
+
+
+def _parse_response_command(response_mode, response_pid):
+    command_mode = response_mode - _OBD_RESPONSE_ID
+    command_pid = response_pid
+    command = OBDCommand(command_mode, command_pid)
+    return command
+
+
+def _convert_raw_response_to_words(raw_response):
+    words_as_str = raw_response.split()
+    words = [int(word, 16) for word in words_as_str]
+    return words
---
+++ tests/test_obd.py
@@ -0,0 +1,70 @@
+from unittest import skip
+
+from nose.tools import assert_false
+from nose.tools import assert_raises
+from nose.tools import eq_
+from nose.tools import ok_
+
+from elm327.obd import NoDataReceivedError
+from elm327.obd import OBDCommand
+from elm327.obd import OBDResponse
+from elm327.obd import UnsupportedCommandError
+
+
+_OBD_COMMAND = OBDCommand(0x01, 0x10)
+
+
+class TestOBDCommand(object):
+
+    def test_conversion_to_hex_words(self):
+        hex_words = _OBD_COMMAND.to_hex_words()
+        eq_(("01", "10"), hex_words)
+
+    def test_conversion_to_pretty_hex_words(self):
+        hex_words = _OBD_COMMAND.to_hex_words(True)
+        eq_(("0x01", "0x10"), hex_words)
+
+    def test_repr(self):
+        eq_('OBDCommand(mode=0x01, pid=0x10)', repr(_OBD_COMMAND))
+
+    def test_equality(self):
+        ok_(_OBD_COMMAND == _OBD_COMMAND)
+        ok_(_OBD_COMMAND == OBDCommand(_OBD_COMMAND.mode, _OBD_COMMAND.pid))
+
+        assert_false(_OBD_COMMAND == OBDCommand(_OBD_COMMAND.mode, 0xAA))
+        assert_false(_OBD_COMMAND == OBDCommand(0xAA, _OBD_COMMAND.pid))
+
+        assert_false(_OBD_COMMAND == None)
+
+
+class TestOBDResponseConstruction(object):
+
+    def test_requested_command_identification(self):
+        """
+        The command the resulted in the response can be identified from the
+        response.
+
+        """
+        response = OBDResponse.make("41 10 00 00 00")
+        eq_(_OBD_COMMAND, response.command)
+
+    def test_response_raw_data(self):
+        response = OBDResponse.make("41 10 0A 0B 0C D0")
+        expected_raw_data = (10, 11, 12, 208)
+        eq_(expected_raw_data, response.raw_data)
+
+    def test_no_data_received(self):
+        """
+        An exception is raised when a "NO DATA" response is received.
+
+        """
+        with assert_raises(NoDataReceivedError):
+            OBDResponse.make("NO DATA")
+
+    def test_unsupported_command_response(self):
+        """
+        An exception is raised when a "?" response is received.
+
+        """
+        with assert_raises(UnsupportedCommandError):
+            OBDResponse.make("?")
